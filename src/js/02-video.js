776599c3bb70033574324ce9cd4aa5f401b115d5
Olga-Dames <113420629+Olga-Dames@users.noreply.github.com>
2023-01-16 22:44:16 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const onPlay = evt => {
+    const timePosition = evt.seconds;
+    localStorage.setItem(STORAGE_KEY, timePosition)
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const getCurrentTime = localStorage.getItem(STORAGE_KEY);
+
+player.setCurrentTime(getCurrentTime);
