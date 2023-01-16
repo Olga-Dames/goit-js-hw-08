840cced5df6da06504bf8de085e059c76fa45828
Olga-Dames <113420629+Olga-Dames@users.noreply.github.com>
2023-01-16 20:58:59 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,51 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items';
+
+const galleryRef = document.querySelector('.gallery')
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryRef.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(galleryItems) { 
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+      `;
+    }).join(''); 
+}
+
+// function createGalleryMarkup(galleryItems) { 
+//     return galleryItems
+//       .reduce((acc, { preview, original, description }) => {
+//         return ` ${acc}
+//         <a class="gallery__item" href="${original}">
+//         <img class="gallery__image" src="${preview}" alt="${description}" />
+//       </a>
+//         `;
+//       }); 
+//   }
+
+galleryRef.addEventListener('click', onClickSimpleLightBox)
+  
+function onClickSimpleLightBox(e) {
+  e.preventDefault();
+
+  if (!e.target.classList.contains('gallery__image')) {
+    return;
+  }
+    const fullSizeImage = e.target.parentNode;
+
+  let lightbox = new SimpleLightbox('.gallery a',
+    {
+      captionsData: 'alt',
+      captionDelay: '250ms'
+      })
+}
+
+
