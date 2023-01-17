e4ac2f363820f3fdd6e37a6acba58b1b2f1a7729
Olga-Dames <113420629+Olga-Dames@users.noreply.github.com>
2023-01-17 09:25:21 +0000

    Deploying to gh-pages from @ Olga-Dames/goit-js-hw-08@4fb021fd5db4a5faba7410910eca7cd2ee3b59b1 🚀

---
+++ 03-feedback.8e7dd7ba.js
@@ -0,0 +1,2 @@
+document.querySelector(".feedback-form").addEventListener("input",(function(t){e[t.target.name]=t.target.value,localStorage.setItem("feedback-form-state",JSON.stringify(e))})),function(){localStorage.getItem("feedback-form-state");console.log("hello")}();const e={};
+//# sourceMappingURL=03-feedback.8e7dd7ba.js.map
---
+++ 03-feedback.e72a061a.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector(".feedback-form").addEventListener("input",(function(o){t[o.target.name]=o.target.value,localStorage.setItem(e,JSON.stringify(t))})),function(){localStorage.getItem(e);console.log("hello")}();var e="feedback-form-state",t={}}();
+//# sourceMappingURL=03-feedback.e72a061a.js.map
