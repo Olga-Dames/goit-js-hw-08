ff334175684a06cced9144313e6b71f1bfa1cd94
Olga-Dames <113420629+Olga-Dames@users.noreply.github.com>
2023-01-17 12:03:27 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+const formRef = document.querySelector('.feedback-form');
 
+formRef.addEventListener('input', onInputChange);
+// formRef.addEventListener('submit', onFormSubmit);
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const userData= {};
+
+// populateFormInputs();
+
+function onInputChange(e) {
+    userData[e.target.name] = e.target.value;
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(userData));
+}
+
+
+// function onFormSubmit(e) {
+//     e.preventDefault();
+
+//     localStorage.removeItem(STORAGE_KEY);
+//     e.target.reset();
+//     console.log(`${formValue}`);
+// }
+
+// function populateFormInputs(e) {
+//     const savedData = localStorage.getItem(STORAGE_KEY);
+
+//     if(savedData) {
+//         // 
+//         console.log(JSON.parse(savedData))
+        
+//     }
+// }
